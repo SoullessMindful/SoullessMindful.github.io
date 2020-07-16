0eb5eaa9a0f1f30f52d7d98d8c1fbdddae0a0f6d
Sławomir Kusiński <skwinged@gmail.com>
2020-07-16 15:23:44 +0200

    The basic structure of landing page

--- soulless-mindful-home/src/App.tsx
+++ soulless-mindful-home/src/App.tsx
@@ -1,26 +1,23 @@
 import React, { ReactElement } from 'react'
-import logo from './logo.svg'
 import './App.css'
 
-function App(): ReactElement {
+export const App = (): ReactElement => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>soulless MindfuL</h1>
       </header>
+      <nav className="App-navigation">
+        <div>My Skills</div>
+        <div>My Projects</div>
+        <div>My Repo</div>
+        <div>My Linkedin</div>
+      </nav>
+      <main className="App-content"></main>
+      <div className="App-footer App-footer-shadow">A</div>
+      <footer className="App-footer App-footer-bottom">
+        Soulless Mindful - Sławomir Kusiński - 2020
+      </footer>
     </div>
   )
 }
-
-export default App
