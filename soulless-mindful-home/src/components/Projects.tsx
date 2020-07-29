b630de6571e0c4d3a37309ef03c858b6053bb845
Sławomir Kusiński <skwinged@gmail.com>
2020-07-29 21:29:58 +0200

    Rendering projects from the project data file

--- soulless-mindful-home/src/components/Projects.tsx
+++ soulless-mindful-home/src/components/Projects.tsx
@@ -1,36 +1,30 @@
-import React, { FunctionComponent } from 'react'
+import React, { FunctionComponent, useState, useEffect } from 'react'
 import { StyledContainer } from './StyledContainer'
 import { Project } from './Project'
 
+const PATH_PROJECTS_DATA = '/projects-data.json'
+
 export const Projects: FunctionComponent = () => {
+  const [projectsData, setProjectsData] = useState<any[]>([])
+
+  useEffect(() => {
+    fetch(PATH_PROJECTS_DATA)
+      .then(async (res) => await res.json())
+      .then((data) => setProjectsData(data))
+      .catch(() => setProjectsData([]))
+  }, [])
+
   return (
     <StyledContainer>
       <h2>These are the projects I have undertaken</h2>
-      <Project
-        name="ParseRiver"
-        description="A monadic parser library, allowing to implement complex parsers using human readable code"
-        tags={['Typescript', 'No Dependencies']}
-        links={[
-          {
-            label: 'Repo',
-            href: 'https://github.com/SoullessMindful/parseriver',
-          },
-          {
-            label: 'NPM',
-            href: 'https://www.npmjs.com/package/parseriver',
-          },
-        ]}
-      />
-      <Project
-        name="Esperanto Parser"
-        description="A grammar analyzer, spellchecker and integrated editor for a constructed language - Esperanto"
-        tags={['Typescript', 'Webpack', 'Sass', 'ParseRiver']}
-      />
-      <Project
-        name="Home Page"
-        description="The very page you are watching right now"
-        tags={['Typescript', 'React', 'Styled Components']}
-      />
+      {projectsData.map(({ name, description, tags, links }: any) => (
+        <Project
+          name={name}
+          description={description}
+          tags={tags}
+          links={links}
+        />
+      ))}
     </StyledContainer>
   )
 }
