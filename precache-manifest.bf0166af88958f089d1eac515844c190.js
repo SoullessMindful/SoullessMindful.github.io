8729d5468ae912b3e240539a6bb7527f43367f0e
Sławomir Kusiński <skwinged@gmail.com>
2020-08-10 14:05:42 +0200

    Build and release

---
+++ precache-manifest.bf0166af88958f089d1eac515844c190.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39e9b59ce178c715f90789cc71115051",
+    "url": "/index.html"
+  },
+  {
+    "revision": "76bf904bba33ad6e58cb",
+    "url": "/static/css/main.0c6a8788.chunk.css"
+  },
+  {
+    "revision": "669639e029dcec8ebfc7",
+    "url": "/static/js/2.04556663.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.04556663.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76bf904bba33ad6e58cb",
+    "url": "/static/js/main.e4ea4eee.chunk.js"
+  },
+  {
+    "revision": "d5ce6d3e65d761cd8d7c",
+    "url": "/static/js/runtime-main.94e9db8a.js"
+  },
+  {
+    "revision": "3154f28d12cf9aa5a0c0f0a62fb27577",
+    "url": "/static/media/CinzelDecorative-Bold.3154f28d.otf"
+  },
+  {
+    "revision": "01f3d4803613ee9556769509a85dba50",
+    "url": "/static/media/FiraCode-Bold.01f3d480.ttf"
+  },
+  {
+    "revision": "d60b1090972c3e6230c555347da880db",
+    "url": "/static/media/FiraCode-Regular.d60b1090.ttf"
+  }
+]);
